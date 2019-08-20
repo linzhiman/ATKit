75e876b150f916e70a782dc47bf46560ca1ce7f7
Aman <zhmlin@gmail.com>
2019-08-20 19:13:39 +0800

    查找不用的方法

--- ATKit/Shell/findUnusedCode.py
+++ ATKit/Shell/findUnusedCode.py
@@ -3,7 +3,15 @@ import re
 import subprocess
 
 
-def get_data_set(command):
+def make_address(address_item_list):
+    address = ''
+    for index in range(len(address_item_list)):
+        address = address + address_item_list[len(address_item_list) - 1 - index]
+    address = address.lstrip('0')
+    return address
+
+
+def get_address_set(command):
     all_lines = subprocess.getoutput(command)
     all_lines = all_lines.splitlines()
     all_set = set()
@@ -14,24 +22,23 @@ def get_data_set(command):
             line_items = line.strip().split(' ')
             # ['68', 'a5', '01', '00', '01', '00', '00', '00', 'e0', 'a5', '01', '00', '01', '00', '00', '00']
             # 10001a568 10001a5e0
-            line_string_l = ''
-            line_string_r = ''
-            line_string_length = len(line_items) // 2
-            line_string_l_index = line_string_length - 1
-            line_string_r_index = len(line_items) - 1
-            for index in range(line_string_length):
-                line_string_l = line_string_l + line_items[line_string_l_index - index]
-                line_string_r = line_string_r + line_items[line_string_r_index - index]
-            line_string_l = line_string_l.lstrip('0')
-            line_string_r = line_string_r.lstrip('0')
-            if len(line_string_l) > 0:
-                all_set.add(line_string_l)
-            if len(line_string_r) > 0:
-                all_set.add(line_string_r)
+            line_items_l = []
+            line_items_r = []
+            if len(line_items) == 8:
+                line_items_l = line_items
+            elif len(line_items) == 16:
+                line_items_l = line_items[0:8]
+                line_items_r = line_items[8:16]
+            line_string = make_address(line_items_l)
+            if len(line_string) > 0:
+                all_set.add(line_string)
+            line_string = make_address(line_items_r)
+            if len(line_string) > 0:
+                all_set.add(line_string)
     return all_set
 
 
-def get_name_dic(command, out_all_name_dic, out_all_super_dic):
+def get_address_classname_dic(command, out_all_name_dic, out_all_super_dic):
     # Contents of (__DATA,__objc_classlist) section
     # 0000000100014c28 0x10001a568 _OBJC_CLASS_$_ATKitTransformViewController
     # ...
@@ -65,34 +72,96 @@ def get_name_dic(command, out_all_name_dic, out_all_super_dic):
                         last_class_find_super = ''
 
 
-def do(object_file_path):
+def get_address_selname_dic(command, out_all_name_dic):
+    # 		      name 0x100011963 setNotifications:
+    # 		     types 0x10001294c v24@0:8@16
+    # 		       imp 0x10000d330 -[ATNotificationUtils setNotifications:]
+    all_lines = subprocess.getoutput(command)
+    all_lines = all_lines.splitlines()
+    rule_namelist = r'^\s+name\s0x(\w+)\s(.+)'
+    rule_sellist = r'^\s+imp\s0x(\w+)\s(.+)'
+    is_in_base_methods = False
+    last_address = ''
+    for line in all_lines:
+        if 'baseMethods 0x' in line:
+            is_in_base_methods = True
+        elif 'baseProtocols 0x' in line:
+            is_in_base_methods = False
+        elif is_in_base_methods:
+            if len(last_address) == 0:
+                match_obj = re.match(rule_namelist, line)
+                if match_obj:
+                    last_address = match_obj.group(1)
+            else:
+                match_obj = re.match(rule_sellist, line)
+                if match_obj:
+                    same_name_list = out_all_name_dic.get(last_address, [])
+                    same_name_list.append(match_obj.group(2))
+                    out_all_name_dic[last_address] = same_name_list
+                    last_address = ''
+
+
+def find_unused_class(object_file_path):
     all_class_name_dic = {}
     all_class_super_dic = {}
-    get_name_dic('otool -o -v %s' % object_file_path, all_class_name_dic, all_class_super_dic)
-    print(all_class_name_dic)
-    print(all_class_super_dic)
+    get_address_classname_dic('otool -o -v %s' % object_file_path, all_class_name_dic, all_class_super_dic)
+    # print(all_class_name_dic)
+    # print(all_class_super_dic)
 
-    all_class_ref_set = get_data_set('otool -s __DATA __objc_classrefs %s' % object_file_path)
+    all_class_ref_set = get_address_set('otool -s __DATA __objc_classrefs %s' % object_file_path)
     all_class_super_ref_set = set()
     for class_ref in all_class_ref_set:
         tmp_class_ref = class_ref
         while tmp_class_ref in all_class_super_dic:
             tmp_class_ref = all_class_super_dic[tmp_class_ref]
             all_class_super_ref_set.add(tmp_class_ref)
-    print(all_class_ref_set)
-    print(all_class_super_ref_set)
+    # print(all_class_ref_set)
+    # print(all_class_super_ref_set)
     all_class_ref_set = all_class_ref_set.union(all_class_super_ref_set)
 
-    all_class_set = get_data_set('otool -s __DATA __objc_classlist %s' % object_file_path)
-    print(all_class_set)
+    all_class_set = get_address_set('otool -s __DATA __objc_classlist %s' % object_file_path)
+    # print(all_class_set)
 
     all_class_unused_set = all_class_set.difference(all_class_ref_set)
-    print(len(all_class_unused_set))
-    print(all_class_unused_set)
+    # print(len(all_class_unused_set))
+    # print(all_class_unused_set)
 
     all_unused_name_list = []
     for key in all_class_unused_set:
         all_unused_name_list.append(all_class_name_dic[key])
+    print('all_unused_name_list')
+    print(len(all_unused_name_list))
     print(all_unused_name_list)
 
-do('./ATKitDemo')
+
+def find_unused_selector(object_file_path):
+    all_sel_set = get_address_set('otool -s __DATA __objc_selrefs %s' % object_file_path)
+    # print(all_sel_set)
+    # print(len(all_sel_set))
+
+    all_sel_name_dic = {}
+    get_address_selname_dic('otool -o -v %s' % object_file_path, all_sel_name_dic)
+    # print(all_sel_name_dic)
+    # print(len(all_sel_name_dic))
+
+    all_same_name_dic = {}
+    for address in all_sel_set:
+        if address in all_sel_name_dic:
+            same_name_list = all_sel_name_dic[address]
+            if len(same_name_list) == 1:
+                del all_sel_name_dic[address]
+            else:
+                all_same_name_dic[address] = same_name_list
+
+    all_maybe_unused_sel_name_list = sorted(all_sel_name_dic.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    print('all_maybe_unused_sel_name_list')
+    print(len(all_maybe_unused_sel_name_list))
+    print(all_maybe_unused_sel_name_list)
+    all_maybe_used_same_sel_name_list = sorted(all_same_name_dic.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    print(len(all_maybe_used_same_sel_name_list))
+    print(all_maybe_used_same_sel_name_list)
+
+
+find_unused_class('./ATKitDemo')
+find_unused_selector('./ATKitDemo')
+
